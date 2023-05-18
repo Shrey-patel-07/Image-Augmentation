cd761d8ecd131291081c78f6a96f999ff4fcfffc
Patel Shrey <Shrey07Patel@gmail.com>
2023-05-18 22:24:45 +0530

    Add SIFT_Feature_Matching.py

--- SIFT_Feature_Matching.py
+++ SIFT_Feature_Matching.py
@@ -1,4 +1,3 @@
-
 import cv2
 import matplotlib.pyplot as plt
 import time
@@ -20,8 +19,8 @@ while cap.isOpened():
 
     start = time.time()
 
-    img1 = cv2.cvtColor(img1, cv2.COLOR_BGR2GRAY)
-    img2 = cv2.cvtColor(img2, cv2.COLOR_BGR2GRAY)
+    # Convert the image to grayscale
+    # img1 = cv2.cvtColor(img1, cv2.COLOR_BGR2GRAY)
 
     keypoints_1, descriptors_1 = sift.detectAndCompute(img1, None)
     keypoints_2, descriptors_2 = sift.detectAndCompute(img2, None)
@@ -35,8 +34,13 @@ while cap.isOpened():
     fps = 1 / totalTime
     print("FPS: {:.0f}".format(fps))
 
+    # Feature Matching in two images
     img3 = cv2.drawMatches(img1, keypoints_1, img2,
-                           keypoints_2, matches[:300], img2, flags=2)
+                           keypoints_2, matches[:120], img2, flags=2)
+
+    # Just Feature Localisation
+    # img3 = cv2.drawKeypoints(img1, keypoints_1, img2)
+
     cv2.putText(img3, f'FPS: {int(fps)}', (20, 450),
                 cv2.FONT_HERSHEY_SIMPLEX, 1.5, (0, 255, 0), 2)
     cv2.imshow('SIFT', img3)
